5dbdecc12773c59e8a1e701b6429cff9d64e45bb
JDR15 <johanrodrigueznacun@gmail.com>
2023-02-21 10:27:39 -0500

    Se agrega Discount

---
+++ src/Discount.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Discount {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        int numero = (int) (Math. random()*5+1);
+
+        double buy, red, blue, yellow;
+        
+        System.out.println(" Ingresa el valor de la compra ");
+        buy = lectura.nextInt();
+
+
+        if(numero == 1){
+            red = (buy*10)/100;
+            System.out.println(" Sacaste la bolita roja tu descuento es de: " +red);
+        }
+        else if(numero == 2){
+            blue =(buy*30)/100;
+            System.out.println(" Sacaste la bolita azul tu descuento es de: " +blue);
+        }
+        else if(numero == 3){
+            yellow =(buy*50)/100;
+            System.out.println(" Sacaste la bolita amarilla tu descuento es de: " +yellow);
+        }
+        else if(numero == 4){
+            System.out.println(" Sacaste la bolita blanca tu compra es gratis ");
+        }
+        else if(numero == 5){
+            System.out.println(" Lo sentimos no obtuviste ningun descuento ");
+        }
+
+
+        lectura.close();
+    }
+}

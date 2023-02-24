27f5f6cc7bf8eb89c2c78c714bba1a876b6e61a9
JDR15 <johanrodrigueznacun@gmail.com>
2023-02-24 12:06:32 -0500

    Se agrega Budget

--- src/Budget2.java
+++ src/Budget2.java
@@ -4,32 +4,23 @@ public class Budget2 {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
 
-        int spent, budget, totals, op;
+        int spent, budget, op, contador ;
 
         op = 1;
+        budget = 100000;
+        contador = 0; 
 
-        while(op==1){
+        while(op==1 && contador<=3){
             System.out.println(" Ingrese el gasto ");
             spent = lectura.nextInt();
+            budget = budget-spent;
+            contador = contador+1;
+    
+            System.out.println(" El gasto fue de " +spent+ " y su presupuesto es de "+budget);
 
             System.out.println(" Desea registrar otro gasto 1.Si 2.No");
+            op = lectura.nextInt();
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

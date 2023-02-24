bca690b2b11907e327196309981e596c4e057433
JDR15 <johanrodrigueznacun@gmail.com>
2023-02-24 11:07:59 -0500

    Se agrega Grade

---
+++ src/Grade.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+
+        double nota,promedio, resultados;
+       
+        resultados = 0;
+        for(int x=1; x<=4; x++){
+            System.out.println(" Digite sus notas ");
+            nota = lectura.nextDouble();
+             
+            promedio = nota/4;
+            resultados = resultados +promedio;
+        }
+ 
+        if(resultados<=2.9){
+            System.out.println(" Reprobaste la asignatura ");
+        }
+        else if(resultados<=4.0){
+            System.out.println(" Pasaste raspando la asignatura ");
+        }
+        else if(resultados>=4.0){
+            System.out.println(" Aprobaste con buenos resultados ");
+        }
+       
+        lectura.close();
+    }
+}
